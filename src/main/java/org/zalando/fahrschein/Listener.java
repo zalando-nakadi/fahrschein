aacbdfd5af470547ed8fa1f4939ffde3bc74ce11
Lukas Niemeier <lukas.niemeier@zalando.de>
2016-07-04 08:57:04 +0200

    Abort connection per default on mapping error

--- src/main/java/org/zalando/fahrschein/Listener.java
+++ src/main/java/org/zalando/fahrschein/Listener.java
@@ -1,12 +1,14 @@
 package org.zalando.fahrschein;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import java.io.IOException;
 import java.util.List;
 
 public interface Listener<T> {
     void onEvent(final List<T> event) throws IOException, EventAlreadyProcessedException;
 
-    default void onError(final IOException e) throws IOException {
-        // no operation
+    default void onError(final JsonMappingException e) throws JsonMappingException {
+        throw e;
     }
 }
