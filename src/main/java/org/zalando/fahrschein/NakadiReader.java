9404c085de44cfb2cd3b5911231bb347d2278992
Lukas Niemeier <lukas.niemeier@zalando.de>
2016-07-04 13:49:51 +0200

    Simplfy mapping of events array

--- src/main/java/org/zalando/fahrschein/NakadiReader.java
+++ src/main/java/org/zalando/fahrschein/NakadiReader.java
@@ -3,8 +3,10 @@ package org.zalando.fahrschein;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import com.google.common.collect.Iterables;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -124,26 +126,28 @@ public class NakadiReader<T> {
 
     private List<T> readEvents(ObjectReader objectReader, JsonParser jsonParser) throws IOException {
         expectToken(jsonParser, JsonToken.START_ARRAY);
+        jsonParser.clearCurrentToken();
+
+        final Iterator<T> eventIterator = objectReader.readValues(jsonParser, eventClass);
 
         final List<T> events = new ArrayList<>();
+        while (eventIterator.hasNext()) {
+            readEvent(eventIterator, events);
+        }
+        return events;
+    }
 
-        if (jsonParser.nextToken() == JsonToken.START_OBJECT) {
-            final Iterator<T> eventIterator = objectReader.readValues(jsonParser, eventClass);
-            while (eventIterator.hasNext()) {
-                try {
-                    events.add(eventIterator.next());
-                } catch (final Exception e) {
-                    final Throwable cause = e.getCause();
-                    if (cause instanceof IOException) {
-                        listener.onError((IOException) cause);
-                    } else {
-                        throw e;
-                    }
-                }
+    private void readEvent(final Iterator<T> source, final List<T> target) throws JsonMappingException {
+        try {
+            target.add(source.next());
+        } catch (final RuntimeJsonMappingException e) {
+            final Throwable cause = e.getCause();
+            if (cause instanceof JsonMappingException) {
+                listener.onError((JsonMappingException) cause);
+            } else {
+                throw e;
             }
         }
-
-        return events;
     }
 
     public void run() throws IOException {
