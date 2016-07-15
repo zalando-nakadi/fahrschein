02ac07c3ac2f4aac84a3c1d70e845c74684709ce
Jörn Horstmann <joern.horstmann@zalando.de>
2016-07-15 11:35:31 +0200

    Add javadoc to updatePartitions method

--- src/main/java/org/zalando/fahrschein/CursorManager.java
+++ src/main/java/org/zalando/fahrschein/CursorManager.java
@@ -19,7 +19,9 @@ public interface CursorManager {
     Ordering<String> OFFSET_ORDERING = Ordering.natural().nullsFirst().onResultOf((String offset) -> "BEGIN".equals(offset) ? null : Long.parseLong(offset));
 
     void onSuccess(String eventName, Cursor cursor) throws IOException;
+
     void onError(String eventName, Cursor cursor, Throwable throwable) throws IOException;
+
     Collection<Cursor> getCursors(String eventName) throws IOException;
 
     default void addSubscription(Subscription subscription) {
@@ -36,7 +38,7 @@ public interface CursorManager {
     }
 
     /**
-     * Initializes offsets to start streaming at the oldes available offset (BEGIN).
+     * Initializes offsets to start streaming at the oldest available offset (BEGIN).
      */
     default void fromOldestAvailableOffset(String eventName, List<Partition> partitions) throws IOException {
         for (Partition partition : partitions) {
@@ -44,6 +46,9 @@ public interface CursorManager {
         }
     }
 
+    /**
+     * Updates offsets in case the currently stored offset is no longer available. Streaming will start at the oldest available offset (BEGIN) to minimize the amount of events skipped.
+     */
     default void updatePartitions(String eventName, List<Partition> partitions) throws IOException {
 
         final Map<String, Cursor> cursorsByPartition = getCursors(eventName).stream().collect(Collectors.toMap(Cursor::getPartition, c -> c));
