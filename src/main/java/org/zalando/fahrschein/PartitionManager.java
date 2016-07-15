f00b1b8bab974137319c5b5dbb5312cec5c477e5
Jörn Horstmann <joern.horstmann@zalando.de>
2016-07-15 15:21:00 +0200

    Small javadoc improvements

--- src/main/java/org/zalando/fahrschein/PartitionManager.java
+++ src/main/java/org/zalando/fahrschein/PartitionManager.java
@@ -9,17 +9,19 @@ import java.util.concurrent.TimeUnit;
 
 public interface PartitionManager {
     /**
+     * Locks partitions for one event. When running consumers on multiple nodes they should try to lock partitions so that events are only processed by one consumer.
      *
      * @param eventName Name of the event
      * @param partitions The partitions to lock
      * @param lockedBy Name of one node of the consuming application
      * @param timeout Timeout after that the lock will automatically be released
      * @param timeoutUnit Time unit for the timeout
-     * @return Whether locking succeeded, i.e. the lock was not held by anyone or the existing lock expired. The current implementations will also return true if the lock is already held by the same node, but not update the expiry time.
+     * @return A {@code Lock} instance containing the list of locked partitions if locking succeded, {@code Optional.empty()} otherwise
      */
     Optional<Lock> lockPartitions(String eventName, List<Partition> partitions, String lockedBy, long timeout, TimeUnit timeoutUnit);
 
     /**
+     * Unlocks previously locked partitions.
      *
      * @param lock The lock object returned from {@link PartitionManager#lockPartitions(String, List, String, long, TimeUnit)}
      */
