a8fca8a9394394f96585cae2ae2647f56c07d341
Tobias Mühl <tobias.muehl@zalando.de>
2016-07-20 17:57:36 +0200

    Can also be package protected

--- src/main/java/org/zalando/fahrschein/NakadiReaderFactory.java
+++ src/main/java/org/zalando/fahrschein/NakadiReaderFactory.java
@@ -16,14 +16,14 @@ class NakadiReaderFactory {
     private final CursorManager cursorManager;
     private final ObjectMapper objectMapper;
 
-    public NakadiReaderFactory(final ClientHttpRequestFactory clientHttpRequestFactory, final BackoffStrategy backoffStrategy, final CursorManager cursorManager, final ObjectMapper objectMapper) {
+    NakadiReaderFactory(final ClientHttpRequestFactory clientHttpRequestFactory, final BackoffStrategy backoffStrategy, final CursorManager cursorManager, final ObjectMapper objectMapper) {
         this.clientHttpRequestFactory = clientHttpRequestFactory;
         this.backoffStrategy = backoffStrategy;
         this.cursorManager = cursorManager;
         this.objectMapper = objectMapper;
     }
 
-    public <T> NakadiReader createReader(final URI uri, final String eventName, final Optional<Subscription> subscription,
+    <T> NakadiReader createReader(final URI uri, final String eventName, final Optional<Subscription> subscription,
             final Class<T> eventType, final Listener<T> listener, @Nullable final MetricsCollector metricsCollector) {
         final NakadiReader<T> nakadiReader = new NakadiReader<>(uri, clientHttpRequestFactory, backoffStrategy, cursorManager, objectMapper,
                 eventName, subscription, eventType, listener, metricsCollector);
