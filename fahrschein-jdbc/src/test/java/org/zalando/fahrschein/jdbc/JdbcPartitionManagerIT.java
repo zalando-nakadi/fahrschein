e3549c3ff31078ee8b2a77a69e5192a58e2b5628
Jörn Horstmann <joern.horstmann@zalando.de>
2017-07-21 14:23:37 +0200

    Fix deprecation warning with new spring version

--- fahrschein-jdbc/src/test/java/org/zalando/fahrschein/jdbc/JdbcPartitionManagerIT.java
+++ fahrschein-jdbc/src/test/java/org/zalando/fahrschein/jdbc/JdbcPartitionManagerIT.java
@@ -3,6 +3,7 @@ package org.zalando.fahrschein.jdbc;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -12,7 +13,7 @@ import org.zalando.fahrschein.test.AbstractPartitionManagerTest;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = LocalPostgresConfiguration.class)
-@TransactionConfiguration(defaultRollback = true)
+@Rollback
 @Transactional
 public class JdbcPartitionManagerIT extends AbstractPartitionManagerTest {
 
