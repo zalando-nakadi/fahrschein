44d26342c6770148cb1f7cc34c8816489bb1448b
Jörn Horstmann <joern.horstmann@zalando.de>
2017-08-09 18:58:01 +0200

    Write dates as strings

--- fahrschein/src/main/java/org/zalando/fahrschein/DefaultObjectMapper.java
+++ fahrschein/src/main/java/org/zalando/fahrschein/DefaultObjectMapper.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
@@ -16,6 +17,7 @@ class DefaultObjectMapper {
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         objectMapper.registerModules(new Jdk8Module(), new ParameterNamesModule(), new JavaTimeModule());
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         INSTANCE = objectMapper;
     }
 
