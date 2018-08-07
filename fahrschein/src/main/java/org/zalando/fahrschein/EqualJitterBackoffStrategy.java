fcbf8a6c8cc6e2687f17a579e71395f5b4b54dac
Ancelot Pinto <ancelot.pinto@zalando.de>
2018-08-07 19:22:46 +0200

    Equal & FullJitterBackoffStrategy
    Implementation of:
    https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/

---
+++ fahrschein/src/main/java/org/zalando/fahrschein/EqualJitterBackoffStrategy.java
@@ -0,0 +1,32 @@
+package org.zalando.fahrschein;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+
+public class EqualJitterBackoffStrategy extends ExponentialBackoffStrategy {
+    private static final Logger LOG = LoggerFactory.getLogger(EqualJitterBackoffStrategy.class);
+    private final Random random = new Random();
+
+    public EqualJitterBackoffStrategy() {
+    }
+
+    public EqualJitterBackoffStrategy(int initialDelay, double backoffFactor, long maxDelay, int maxRetries) {
+        super(initialDelay, backoffFactor, maxDelay, maxRetries);
+    }
+
+    private EqualJitterBackoffStrategy(EqualJitterBackoffStrategy other) {
+        super(other);
+    }
+
+    public EqualJitterBackoffStrategy withMaxRetries(int maxRetries) {
+        return new EqualJitterBackoffStrategy(this.withMaxRetries(maxRetries));
+    }
+
+    protected long calculateDelay(double count) {
+        final long ceil = super.calculateDelay(count);
+        return (ceil / 2) + (long)(random.nextDouble() * ceil / 2);
+    }
+}
