b395e53ea8d68043000ab7faf861b88373c14f3c
Innokenty Shuvalov <innokenty.shuvalov@zalando.de>
2019-07-17 14:37:00 +0200

    #234 - refact AuthorizationBuilder

--- fahrschein/src/main/java/org/zalando/fahrschein/AuthorizationBuilder.java
+++ fahrschein/src/main/java/org/zalando/fahrschein/AuthorizationBuilder.java
@@ -3,11 +3,12 @@ package org.zalando.fahrschein;
 import org.zalando.fahrschein.domain.Authorization;
 import org.zalando.fahrschein.domain.Authorization.AuthorizationAttribute;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
-import static java.util.stream.Collectors.toList;
 import static org.zalando.fahrschein.domain.Authorization.AuthorizationAttribute.ANYONE;
 
 /**
@@ -29,7 +30,7 @@ public class AuthorizationBuilder {
      * No restrictions by default, use {@link #withAdmins} / {@link #withReaders} methods to add them.
      */
     public static AuthorizationBuilder authorization() {
-        return new AuthorizationBuilder(singletonList(ANYONE), singletonList(ANYONE));
+        return new AuthorizationBuilder(emptyList(), emptyList());
     }
 
     public AuthorizationBuilder withAdmins(List<AuthorizationAttribute> admins) {
@@ -49,18 +50,22 @@ public class AuthorizationBuilder {
     }
 
     public AuthorizationBuilder addAdmin(String dataType, String value) {
-        List<AuthorizationAttribute> newAdmins = admins.stream().filter(aa -> !aa.equals(ANYONE)).collect(toList());
+        List<AuthorizationAttribute> newAdmins = new ArrayList<>(admins.size() + 1);
+        newAdmins.addAll(admins);
         newAdmins.add(new AuthorizationAttribute(dataType, value));
         return withAdmins(newAdmins);
     }
 
     public AuthorizationBuilder addReader(String dataType, String value) {
-        List<AuthorizationAttribute> newReaders = readers.stream().filter(aa -> !aa.equals(ANYONE)).collect(toList());;
+        List<AuthorizationAttribute> newReaders = new ArrayList<>(readers.size() + 1);
+        newReaders.addAll(readers);
         newReaders.add(new AuthorizationAttribute(dataType, value));
         return withReaders(newReaders);
     }
 
     public Authorization build() {
+        List<AuthorizationAttribute> admins = this.admins.isEmpty() ? singletonList(ANYONE) : this.admins;
+        List<AuthorizationAttribute> readers = this.readers.isEmpty() ? singletonList(ANYONE) : this.readers;
         return new Authorization(admins, readers);
     }
 }
