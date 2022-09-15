36feaf8b6c0a90aa0281f987819fccf456a7bcb0
Oliver Trosien <otrosien@users.noreply.github.com>
2022-09-15 11:24:08 +0200

    Document CursorManager#onSuccess (#363)
    Closes #338
    
    Signed-off-by: Oliver Trosien <oliver.trosien@zalando.de>
    
    Signed-off-by: Oliver Trosien <oliver.trosien@zalando.de>

--- fahrschein/src/main/java/org/zalando/fahrschein/CursorManager.java
+++ fahrschein/src/main/java/org/zalando/fahrschein/CursorManager.java
@@ -8,11 +8,28 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * Manages cursor offsets for one consumer. One consumer can handle several distinct events.
+ * Manages cursor offsets for one consumer. One consumer can handle several distinct event types.
  */
 public interface CursorManager {
 
+    /**
+     * Commits one cursor on successful event consumption, i.e. if
+     * {@link Listener#accept(List)}} did not throw an exception.
+     * @param eventName event name
+     * @param cursor    cursor to commit
+     * @throws IOException in case of errors during committing
+     */
     void onSuccess(String eventName, Cursor cursor) throws IOException;
+
+    /**
+     * Commits a list of cursors on successful event consumption for multiple cursors
+     * of the same event type. This is useful to reset a consumer to a point in time
+     * like the beginning or the end of an event type.
+     *
+     * @param eventName event name
+     * @param cursors    cursors to commit
+     * @throws IOException in case of errors during committing
+     */
     void onSuccess(String eventName, List<Cursor> cursors) throws IOException;
 
     Collection<Cursor> getCursors(String eventName) throws IOException;
