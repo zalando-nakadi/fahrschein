cfd8af3b4e87b470bdd9077f67c7c2d8be698aaa
Rob Williamson <rob.williamson@zalando.de>
2023-07-11 12:13:09 +0200

    Verify Consumer Config Validation
    Instead of simply logging the output.

--- fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/nakadi/config/properties/ConsumerConfigValidatorTest.java
+++ fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/nakadi/config/properties/ConsumerConfigValidatorTest.java
@@ -3,7 +3,12 @@ package org.zalando.spring.boot.fahrschein.nakadi.config.properties;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.ValidationUtils;
+
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConsumerConfigValidatorTest {
 
@@ -12,8 +17,15 @@ public class ConsumerConfigValidatorTest {
         ConsumerConfig cc = new ConsumerConfig();
         Errors errors = new BeanPropertyBindingResult(cc, "first");
         ValidationUtils.invokeValidator(new ConsumerConfigValidator(), cc, errors);
-        System.out.println("HAS ERRORS : " + errors.hasErrors());
-        System.out.println(errors.toString());
+
+        final var expectedErrors = new String[]{
+                "Field error in object 'first' on field 'id': rejected value [null]; codes [field.required.first.id,field.required.id,field.required.java.lang.String,field.required]; arguments []; default message [null]",
+                "Field error in object 'first' on field 'nakadiUrl': rejected value [null]; codes [field.required.first.nakadiUrl,field.required.nakadiUrl,field.required.java.lang.String,field.required]; arguments []; default message [null]",
+                "Field error in object 'first' on field 'applicationName': rejected value [null]; codes [field.required.first.applicationName,field.required.applicationName,field.required.java.lang.String,field.required]; arguments []; default message [null]",
+                "Field error in object 'first' on field 'consumerGroup': rejected value [null]; codes [field.required.first.consumerGroup,field.required.consumerGroup,field.required.java.lang.String,field.required]; arguments []; default message [null]"
+        };
+        assertThat(errors.getAllErrors().stream().map(ObjectError::toString).toList().toArray())
+                .isEqualTo(expectedErrors);
     }
 
 }
