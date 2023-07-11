280aadbe11ed0242abaab408d7de2ebe81ad535e
Rob Williamson <rob.williamson@zalando.de>
2023-07-11 12:18:53 +0200

    Assert That ObjectMapper Creation Works
    Instead of simply executing the code.

--- fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/test/ObjectMapperFactoryAccessibleTest.java
+++ fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/test/ObjectMapperFactoryAccessibleTest.java
@@ -1,13 +1,23 @@
 package org.zalando.spring.boot.fahrschein.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zalando.spring.boot.fahrschein.nakadi.config.ObjectMapperFactory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ObjectMapperFactoryAccessibleTest {
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        objectMapper = ObjectMapperFactory.create();
+    }
 
     @Test
     public void testCreateObjectMapper() {
-        ObjectMapperFactory.create();
+        assertThat(objectMapper).isNotNull();
     }
 
 }
