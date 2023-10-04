1e367c840e10e280165dc9e76c55b45cdd318967
Oliver Trosien <oliver.trosien@zalando.de>
2023-10-04 13:41:51 +0200

    Discover jackson modules via classpath introspection

--- fahrschein-spring-boot-starter/src/main/java/org/zalando/spring/boot/fahrschein/nakadi/config/ObjectMapperFactory.java
+++ fahrschein-spring-boot-starter/src/main/java/org/zalando/spring/boot/fahrschein/nakadi/config/ObjectMapperFactory.java
@@ -5,18 +5,16 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.kotlin.KotlinModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 public class ObjectMapperFactory {
 
     public static ObjectMapper create() {
-        final ObjectMapper objectMapper = new ObjectMapper();
+        final ObjectMapper objectMapper = Jackson2ObjectMapperBuilder
+                .json()
+                .findModulesViaServiceLoader(true).build();
         objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        objectMapper.registerModules(new Jdk8Module(), new ParameterNamesModule(), new JavaTimeModule(), new KotlinModule.Builder().build());
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
