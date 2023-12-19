64515cde793e4e03b9dc37731d36869c8a6fba52
Oliver Trosien <otrosien@users.noreply.github.com>
2023-12-19 13:32:52 +0100

    Upgrade to latest versions (#454)

--- fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/nakadi/ExampleApplication.java
+++ fahrschein-spring-boot-starter/src/test/java/org/zalando/spring/boot/fahrschein/nakadi/ExampleApplication.java
@@ -6,34 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.SchedulingConfigurer;
-import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 @EnableScheduling
-public class ExampleApplication implements SchedulingConfigurer {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
 
-	@Override
-	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
-		taskRegistrar.setScheduler(taskScheduler());
-
-	}
-
 	@Bean(name = "tracer")
 	public Tracer tracer() {
 		return TracerProvider.noop().get("testing");
-	}
-
-	@Bean(destroyMethod = "shutdown")
-	public Executor taskScheduler() {
-		return Executors.newScheduledThreadPool(6);
 	}
 
 }
